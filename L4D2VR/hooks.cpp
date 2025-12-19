96c7228374d9faf7bd4076924382c5a36d36b765
Keyou <liu547161153@yahoo.com>
2025-12-19 19:16:58 +0800

    Revert "Force right-hand aim during special-infected pre-warning and blind-spot warnings"

--- L4D2VR/hooks.cpp
+++ L4D2VR/hooks.cpp
@@ -686,7 +686,6 @@ void Hooks::dDrawModelExecute(void* ecx, void* edx, void* state, const ModelRend
 		const auto infectedType = m_VR->GetSpecialInfectedType(modelName);
 		if (infectedType != VR::SpecialInfectedType::None)
 		{
-			m_VR->RefreshSpecialInfectedPreWarning(info.origin);
 			m_VR->RefreshSpecialInfectedBlindSpotWarning(info.origin);
 			m_VR->DrawSpecialInfectedArrow(info.origin, infectedType);
 		}
