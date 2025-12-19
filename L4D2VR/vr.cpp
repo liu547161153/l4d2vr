a611ccb79f57b3433cbee2a15b1d76343ee82a5a
Keyou <liu547161153@yahoo.com>
2025-12-19 21:01:40 +0800

    Make crouch hold auto-aim momentary

--- L4D2VR/vr.cpp
+++ L4D2VR/vr.cpp
@@ -1163,6 +1163,32 @@ void VR::ProcessInput()
         crouchJustPressed = false;
     }
 
+    if (!crouchButtonDown)
+    {
+        m_SpecialInfectedPreWarningCrouchHoldActive = false;
+        if (m_SpecialInfectedPreWarningAutoAimEnabled)
+        {
+            m_SpecialInfectedPreWarningAutoAimEnabled = false;
+        }
+    }
+    else
+    {
+        if (!m_SpecialInfectedPreWarningCrouchHoldActive)
+        {
+            m_SpecialInfectedPreWarningCrouchHoldActive = true;
+            m_SpecialInfectedPreWarningCrouchHoldStart = currentTime;
+        }
+
+        if (m_SpecialInfectedPreWarningAutoAimConfigEnabled)
+        {
+            const float holdSeconds = std::chrono::duration<float>(currentTime - m_SpecialInfectedPreWarningCrouchHoldStart).count();
+            if (holdSeconds >= m_SpecialInfectedPreWarningAutoAimCrouchHoldDuration)
+            {
+                m_SpecialInfectedPreWarningAutoAimEnabled = true;
+            }
+        }
+    }
+
     if (primaryAttackDown)
     {
         m_Game->ClientCmd_Unrestricted("+attack");
@@ -1241,14 +1267,6 @@ void VR::ProcessInput()
         else
         {
             m_CrouchToggleActive = !m_CrouchToggleActive;
-            if (m_SpecialInfectedPreWarningAutoAimConfigEnabled)
-            {
-                m_SpecialInfectedPreWarningAutoAimEnabled = !m_SpecialInfectedPreWarningAutoAimEnabled;
-            }
-            else
-            {
-                m_SpecialInfectedPreWarningAutoAimEnabled = false;
-            }
             ResetPosition();
         }
     }
@@ -3226,6 +3244,7 @@ void VR::ParseConfigFile()
     m_SpecialInfectedPreWarningAutoAimConfigEnabled = getBool("SpecialInfectedPreWarningAutoAimEnabled", m_SpecialInfectedPreWarningAutoAimConfigEnabled);
     if (!m_SpecialInfectedPreWarningAutoAimConfigEnabled)
         m_SpecialInfectedPreWarningAutoAimEnabled = false;
+    m_SpecialInfectedPreWarningAutoAimCrouchHoldDuration = std::max(0.0f, getFloat("SpecialInfectedPreWarningAutoAimCrouchHoldDuration", m_SpecialInfectedPreWarningAutoAimCrouchHoldDuration));
     m_SpecialInfectedPreWarningDistance = std::max(0.0f, getFloat("SpecialInfectedPreWarningDistance", m_SpecialInfectedPreWarningDistance));
     m_SpecialInfectedWarningSecondaryHoldDuration = std::max(0.0f, getFloat("SpecialInfectedWarningSecondaryHoldDuration", m_SpecialInfectedWarningSecondaryHoldDuration));
     m_SpecialInfectedWarningPostAttackDelay = std::max(0.0f, getFloat("SpecialInfectedWarningPostAttackDelay", m_SpecialInfectedWarningPostAttackDelay));
